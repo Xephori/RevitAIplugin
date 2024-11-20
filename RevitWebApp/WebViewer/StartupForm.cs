07673f5af0c8c36627d510fd264fe182268be3cd
Xephori <whatforeverla62@gmail.com>
2024-11-20 16:51:40 +0800

    added namespace

--- RevitWebApp/WebViewer/StartupForm.cs
+++ RevitWebApp/WebViewer/StartupForm.cs
@@ -1,19 +1,20 @@
 using System;
 using System.Windows.Forms;
 
-public partial class StartupForm : Form
-{
-    public bool UseLocalhost { get; private set; }
-
-    public StartupForm()
+namespace RevitWebApp
+    public partial class StartupForm : Form
     {
-        InitializeComponent();
-    }
+        public bool UseLocalhost { get; private set; }
 
-    private void openButton_Click(object sender, EventArgs e)
-    {
-        UseLocalhost = useLocalhostCheckbox.Checked;
-        this.DialogResult = DialogResult.OK;
-        this.Close();
+        public StartupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            UseLocalhost = useLocalhostCheckbox.Checked;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
     }
-}
