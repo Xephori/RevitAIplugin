dea826c43a1d074a1a0d7e58422fd5747bf7dbfa
Xephori <whatforeverla62@gmail.com>
2024-11-28 16:02:38 +0800

    Changing ui

--- RevitDataExtractor/UserForm.Designer.cs
+++ RevitDataExtractor/UserForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace WallDataPlugin
+﻿using System;
+using System.IO;
+
+namespace WallDataPlugin
 {
     partial class UserForm
     {
@@ -7,6 +10,8 @@
         private System.Windows.Forms.CheckBox chkLaunchLocalhost;
         private System.Windows.Forms.Button btnLaunchWebUI;
         private System.Windows.Forms.Label lblWebLink;
+        private System.Windows.Forms.PictureBox pictureBoxGif;
+       
 
         public void InitializeComponent()
         {
@@ -14,6 +19,8 @@
             this.chkLaunchLocalhost = new System.Windows.Forms.CheckBox();
             this.btnLaunchWebUI = new System.Windows.Forms.Button();
             this.lblWebLink = new System.Windows.Forms.Label();
+            this.pictureBoxGif = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGif)).BeginInit();
             this.SuspendLayout();
             // 
             // txtWebLink
@@ -57,15 +64,29 @@
             this.lblWebLink.Text = "Hosted Web Link:";
             this.lblWebLink.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
+            // pictureBoxGif
+            // 
+            this.pictureBoxGif.Location = new System.Drawing.Point(500, 250);
+            this.pictureBoxGif.Name = "pictureBoxGif";
+            this.pictureBoxGif.Size = new System.Drawing.Size(300, 150);
+            this.pictureBoxGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGif.TabIndex = 4;
+            this.pictureBoxGif.TabStop = false;
+            string gifPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "bongo-cat.gif");
+            this.pictureBoxGif.Image = System.Drawing.Image.FromFile(gifPath);
+            //  
             // UserForm
             // 
             this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Controls.Add(this.pictureBoxGif);
             this.Controls.Add(this.lblWebLink);
             this.Controls.Add(this.btnLaunchWebUI);
             this.Controls.Add(this.chkLaunchLocalhost);
             this.Controls.Add(this.txtWebLink);
             this.Name = "UserForm";
             this.Text = "Web UI Startup";
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGif)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
