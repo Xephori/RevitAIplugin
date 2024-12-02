8ad8ba9d59c888bd62b3d01a6d0b0a14c6c3db9f
Xephori <whatforeverla62@gmail.com>
2024-12-02 15:27:37 +0800

    editing user form to remove unnecessary ui

--- RevitDataExtractor/UserForm.Designer.cs
+++ RevitDataExtractor/UserForm.Designer.cs
@@ -10,7 +10,7 @@ namespace WallDataPlugin
         private System.Windows.Forms.CheckBox chkLaunchLocalhost;
         private System.Windows.Forms.Button btnLaunchWebUI;
         private System.Windows.Forms.Label lblWebLink;
-        private System.Windows.Forms.PictureBox pictureBoxGif;
+        //private System.Windows.Forms.PictureBox pictureBoxGif;
        
 
         public void InitializeComponent()
@@ -19,8 +19,8 @@ namespace WallDataPlugin
             this.chkLaunchLocalhost = new System.Windows.Forms.CheckBox();
             this.btnLaunchWebUI = new System.Windows.Forms.Button();
             this.lblWebLink = new System.Windows.Forms.Label();
-            this.pictureBoxGif = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGif)).BeginInit();
+            //this.pictureBoxGif = new System.Windows.Forms.PictureBox();
+            //((System.ComponentModel.ISupportInitialize)(this.pictureBoxGif)).BeginInit();
             this.SuspendLayout();
             // 
             // txtWebLink
@@ -63,30 +63,29 @@ namespace WallDataPlugin
             this.lblWebLink.TabIndex = 3;
             this.lblWebLink.Text = "Hosted Web Link:";
             this.lblWebLink.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            // 
-            // pictureBoxGif
-            // 
-            this.pictureBoxGif.Location = new System.Drawing.Point(500, 250);
-            this.pictureBoxGif.Name = "pictureBoxGif";
-            this.pictureBoxGif.Size = new System.Drawing.Size(300, 150);
-            this.pictureBoxGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBoxGif.TabIndex = 4;
-            this.pictureBoxGif.TabStop = false;
-            string gifPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "bongo-cat.gif");
-            this.pictureBoxGif.Image = System.Drawing.Image.FromFile(gifPath);
+            //// 
+            //// pictureBoxGif
+            //// 
+            //this.pictureBoxGif.Location = new System.Drawing.Point(500, 250);
+            //this.pictureBoxGif.Name = "pictureBoxGif";
+            //this.pictureBoxGif.Size = new System.Drawing.Size(300, 150);
+            //this.pictureBoxGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            //this.pictureBoxGif.TabIndex = 4;
+            //this.pictureBoxGif.TabStop = false;
+            //string gifPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "bongo-cat.gif");
+            //this.pictureBoxGif.Image = System.Drawing.Image.FromFile(gifPath);
             //  
             // UserForm
             // 
             this.ClientSize = new System.Drawing.Size(800, 400);
-            this.Controls.Add(this.pictureBoxGif);
             this.Controls.Add(this.lblWebLink);
             this.Controls.Add(this.btnLaunchWebUI);
             this.Controls.Add(this.chkLaunchLocalhost);
             this.Controls.Add(this.txtWebLink);
             this.Name = "UserForm";
             this.Text = "Web UI Startup";
-
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGif)).EndInit();
+            //this.Controls.Add(this.pictureBoxGif);
+            //((System.ComponentModel.ISupportInitialize)(this.pictureBoxGif)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
